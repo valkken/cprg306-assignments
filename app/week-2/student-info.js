252933ef1c2a098b344b5ce0a74a52894cd38389
valkken <bryancolumbres@gmail.com>
2025-09-11 10:51:11 -0600

    Added StudentInfo Component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Jake Columbres</h1>
+            <a href="https://github.com/valkken/cprg306-assignments.git" target="_blank" rel="stylesheet">
+                https://github.com/valkken/cprg306-assignments.git
+            </a>
+          
+        </main>
+    );
+
+
+}
