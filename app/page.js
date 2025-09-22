fd2622b0987bedf882307a011f0361d9de4190d7
valkken <bryancolumbres@gmail.com>
2025-09-22 14:09:43 -0600

    Finished page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+      <h1 className="font-extrabold text-3xl font-serif">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2" className="text-xl font-bold font-serif">Week 2 Assignment</Link>
+      <br />
+      <Link href="/week-3" className="text-xl font-bold font-serif">Week 3 Assignment</Link>
 
     </main>
   );
---
+++ app/week-3/item.js
@@ -0,0 +1,21 @@
+
+export default function Item({item}) {
+    const { name, quantity, category } = item;
+   return (
+
+
+    <div className="flex justify-center items-center  text-black font-serif">
+        
+        <ul className="text-center min-w-[250px] max-w-[500px] min-h-[50px] max-h-[500px] p-1 m-2 bg-gray-400 rounded-lg shadow-lg">
+            <li>
+                <h1 className="font-extrabold text-2x1"> {item.name} </h1> 
+                <p>Buy {item.quantity} in {item.category}</p>
+                
+            </li>   
+        </ul>
+
+    </div>
+    );
+
+}
+
