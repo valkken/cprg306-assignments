be3fc7dc6f525aaff39aa9d772dc609c322c4d75
valkken <bryancolumbres@gmail.com>
2025-10-02 10:18:04 -0600

    Fixed output for button

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -27,16 +27,19 @@ export default function NewItem() {
     return (
 
         <div className="flex justify-center items-center gap-3 bg-white p-3 rounded mx-auto w-40 h-15 shadow ">
-            <div className="text-black bg-gray-300 px-4 py-2 rounded ">{quantity}</div>
+            <div className="text-black  bg-gray-300 h-10 flex items-center justify-center w-13 min-w-10 rounded ">{quantity}</div>
            
            
             <button 
             onClick={decrement} className={
-                quantity == 1 ? "bg-gray-200 font-bold text-black px-4 py-2 rounded-lg transition cursor-not-allowed" :
-                "bg-blue-500 font-bold text-black hover:bg-blue-950 active:bg-gray-700 px-4 py-2 rounded-lg transition"
+                quantity == 1 ? "bg-gray-200 font-bold text-black px-4 py-2 rounded-lg  cursor-not-allowed" :
+                "bg-blue-500 font-bold text-black hover:bg-blue-950 active:bg-gray-700 px-4 py-2 rounded-lg "
+            
             }
             >
-              -
+            
+                -
+
             </button>
            
             <button 
